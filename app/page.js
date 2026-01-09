9a30407edbebbb78a116e523bc077c0d261b2cca
Krishn <22amtics070@gmail.com>
2026-01-09 15:34:44 +0530

    Installed Shadcn and Dark Mode

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>Hello
-      <Button>Crack it</Button>
+      <Button>Nice</Button>
     </div>
   );
 }
